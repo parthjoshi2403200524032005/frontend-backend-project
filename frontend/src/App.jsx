0dc08905e99d6543bef2ee84bd2eaa7f7b42b3b0
Parth <parthjohi009@gmail.com>
2024-01-09 21:15:07 +0530

    add

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,23 +5,20 @@ import axios from "axios";
 function App() {
   const [jokes, setJokes] = useState([]);
   useEffect(() => {
-    axios.get("/api/jokes").then((response) => {
-      setJokes(response.data);}).catch((error) => {
-        console.log(error);
-      })
-  })
+    axios.get("/api/jokes").then((response) => {setJokes(response.data);}).catch((error) =>{console.log(error); })})
  
   return (
     <>
       <h1>CODE WITH ME</h1>
       <p>JOKES: {jokes.length}</p>
-      {jokes.map((joke, index) => (
+      {jokes.map((joke) => (
         <div key={joke.id}>
           {" "}
           <h3>{joke.title}</h3>
           <p>{joke.content}</p>
         </div>
       ))}
+      
     </>
   );
 }
